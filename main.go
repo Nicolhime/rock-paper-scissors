15ba5f4f0c9ec0ba650810f7c3d4e7d03635f528
Royce Doughty <117875050+Nicolhime@users.noreply.github.com>
2025-08-27 13:44:45 -0500

    removed other files (figured they werent needed at all), added switch case to determine who wins

--- main.go
+++ main.go
@@ -2,11 +2,49 @@ package main
 
 import (
 	"fmt"
-	fight "github.com/Nicolhime/rock-paper-scissors/fight"
+	"math/rand"
 )
 
 func main() {
+	var player string
 	fmt.Println("Welcome to Rock Paper Scissors!")
-	fmt.Println(fight.Fight())
+	fmt.Println("Please input either 'rock', 'paper', or 'scissors'")
+	fmt.Scan(&player)                    //stores player's input
+	computer := computer()               //stores computer's input from "computer" function
+	fmt.Println(fight(player, computer)) //sends both inputs to the "fight" function and prints the final result of the game
+}
 
+func computer() string {
+	rps := []string{"rock", "paper", "scissors"}
+	return (rps[rand.Intn(len(rps))]) //randomly chooses an option from the slice above and returns it
+}
+
+func fight(player string, computer string) string {
+	var output string
+
+	switch {
+	case player == computer:
+		fmt.Println("Player chose:", player, ",", "Computer chose:", computer) //if both inputs are the same, output tie
+		output = "Its a Tie!"
+	case player == "rock" && computer == "paper":
+		fmt.Println("Player chose:", player, ",", "Computer chose:", computer) //if computer beats player, output loss
+		output = "Computer Wins!"
+	case player == "paper" && computer == "scissors":
+		fmt.Println("Player chose:", player, ",", "Computer chose:", computer)
+		output = "Computer Wins!"
+	case player == "scissors" && computer == "rock":
+		fmt.Println("Player chose:", player, ",", "Computer chose:", computer)
+		output = "Computer Wins!"
+	case player == "rock" && computer == "scissors":
+		fmt.Println("Player chose:", player, ",", "Computer chose:", computer) //if player beats computer, output win
+		output = "Player Wins!"
+	case player == "paper" && computer == "rock":
+		fmt.Println("Player chose:", player, ",", "Computer chose:", computer)
+		output = "Player Wins!"
+	case player == "scissors" && computer == "paper":
+		fmt.Println("Player chose:", player, ",", "Computer chose:", computer)
+		output = "Player Wins!"
+	}
+
+	return output
 }
